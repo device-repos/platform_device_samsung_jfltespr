2e842a14e298ec01f6cf3aacf427d85e4c38f4fd
Carl Windhausen <cjwindhausen@gmail.com>
2013-11-01 10:27:15 -0400

    4.4 OctoKat KRT16M

--- oct.mk
+++ oct.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 # Inherit some common Oct stuff.
 $(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/oct_jfltespr/jfltespr:4.3.1/JLS36I/user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.3.1 JLS36I release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/oct_jfltespr/jfltespr:4.4/KRT16M/user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.4 KRT16M release-keys"
 
 PRODUCT_NAME := oct_jfltespr
 PRODUCT_DEVICE := jfltespr
