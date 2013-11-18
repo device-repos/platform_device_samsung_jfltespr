ed7b158e46601f7be645e288e2d5af31b3783675
Carl Windhausen <cjwindhausen@gmail.com>
2013-11-18 11:23:15 -0500

    build update

--- oct.mk
+++ oct.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 # Inherit some common Oct stuff.
 $(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/oct_jfltespr/jfltespr:4.4/KRT16M/user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.4 KRT16M release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/oct_jfltespr/jfltespr:4.4/KRT16O/user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.4 KRT16O release-keys"
 
 PRODUCT_NAME := oct_jfltespr
 PRODUCT_DEVICE := jfltespr
