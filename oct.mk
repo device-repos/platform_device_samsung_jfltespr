8c4a596aa8720b3f16b5922073e9fe24c88d480c
treken <chupekevin@gmail.com>
2013-12-02 10:12:36 -0600

    Update oct.mk

--- oct.mk
+++ oct.mk
@@ -1,14 +1,5 @@
 $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 
-# Inherit some common Oct stuff.
- $(call inherit-product, vendor/oct/config/cdma.mk)
-
-# Enhanced NFC
- $(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
-
-# Inherit some common Oct stuff.
-$(call inherit-product, vendor/oct/config/common_full_phone.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/oct_jfltespr/jfltespr:4.4/KRT16O/user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.4 KRT16O release-keys"
 
 PRODUCT_NAME := oct_jfltespr
