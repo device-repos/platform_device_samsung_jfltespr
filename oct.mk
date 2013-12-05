d6770c8c1520a877399d9e058a2edbe77ed4894e
Carl Windhausen <cjwindhausen@gmail.com>
2013-12-05 12:52:48 -0500

    update to 4.4_r1.2 KRT16S

--- oct.mk
+++ oct.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/oct_jfltespr/jfltespr:4.4/KRT16O/user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.4 KRT16O release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/oct_jfltespr/jfltespr:4.4/KRT16S/user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.4 KRT16S release-keys"
 
 PRODUCT_NAME := oct_jfltespr
 PRODUCT_DEVICE := jfltespr
